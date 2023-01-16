a2d7c43133e3f6a716d71c19d90cb743d73c0ddf
Daniel Semblano <dsemblano@gmail.com>
2023-01-16 16:43:20 -0300

    first commit

---
+++ site/web/app/themes/sfco179/tailwind.config.cjs
@@ -0,0 +1,14 @@
+// https://tailwindcss.com/docs/configuration
+module.exports = {
+  content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors: {}, // Extend Tailwind's default colors
+    },
+  },
+  plugins: [],
+};
