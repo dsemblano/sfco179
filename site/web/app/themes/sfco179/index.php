081f539fbfcdb3b5ae8771f020681901e9a22815
Daniel Semblano <dsemblano@gmail.com>
2023-01-16 17:24:10 -0300

    Google Tag Manager head and body code

--- site/web/app/themes/sfco179/index.php
+++ site/web/app/themes/sfco179/index.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    <?php echo \Roots\view('partials/gtaghead')->render(); ?>
   </head>
 
   <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
+    <?php echo \Roots\view('partials/gtagbody')->render(); ?>
     <?php do_action('get_header'); ?>
 
     <div id="app">
