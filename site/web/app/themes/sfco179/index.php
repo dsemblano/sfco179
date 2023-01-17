fbd7a0e60bef66efb7e13ab40d8ec22a9c6204ee
Daniel Semblano <dsemblano@gmail.com>
2023-01-17 17:33:58 -0300

    Favicons

--- site/web/app/themes/sfco179/index.php
+++ site/web/app/themes/sfco179/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    <?php echo \Roots\view('partials/favicons')->render(); ?>
     <?php echo \Roots\view('partials/gtaghead')->render(); ?>
   </head>
 
