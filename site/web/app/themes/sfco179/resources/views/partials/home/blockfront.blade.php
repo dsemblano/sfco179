84363ffcf2ca1868fd94f2fe125f97c93045642b
Daniel Semblano <dsemblano@gmail.com>
2023-10-25 20:05:13 -0300

    eventos link button on the bottom

--- site/web/app/themes/sfco179/resources/views/partials/home/blockfront.blade.php
+++ site/web/app/themes/sfco179/resources/views/partials/home/blockfront.blade.php
@@ -1,17 +1,17 @@
 <section class="bg-home bg-black h-full min-h-screen overflow-hidden">
     <div class="bg-homechild flex justify-center items-center min-h-screen">
         <header class="mx-auto container flex justify-center">
-            <div class="">
-                <div class="flex social-icons justify-around text-white relative mt-14 text-2xl lg:hidden">
+            <div class="flex flex-col justify-center items-center">
+                {{-- <div class="flex social-icons justify-around text-white relative mt-14 text-2xl lg:hidden">
                     <a class="" href="/eventos">179 Eventos</a>
-                </div>   
+                </div>    --}}
                 <h1 class="logo">
                     <picture>
                     <img src="@asset('images/logo.png')" alt="Logo SFCO 179">
                     </picture>
                 </h1>
     
-                <div class="flex social-icons justify-around text-white relative mt-14">
+                <div class="flex social-icons justify-around items-center gap-8 text-white relative mt-14">
                     <a href="https://www.instagram.com/sfco179" target="_blank" aria-label="Link para o Instagram">
                         <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
                             <path
@@ -26,6 +26,10 @@
                                 d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z" />
                         </svg>
                     </a>
+
+                    <button class="button-40" role="button">
+                        <a href="/eventos">179 Eventos</a>
+                    </button>
                 </div>
             </div>
             
