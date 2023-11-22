5afc01b5443933be309c5a4b4d0a9b8bec00c03e
Daniel Semblano <dsemblano@gmail.com>
2023-11-22 04:19:31 -0300

    h1 new font size prose class on pages

--- site/web/app/themes/sfco179/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sfco179/resources/views/partials/page-header.blade.php
@@ -1,3 +1,3 @@
-<div class="page-header mt-12 md:mt-24 md:prose-lg lg:prose-2xl">
-  <h1 class="text-4xl">{!! $title !!}</h1>
+<div class="page-header mt-12 md:mt-24 prose-xl lg:prose-2xl">
+  <h1>{!! $title !!}</h1>
 </div>
