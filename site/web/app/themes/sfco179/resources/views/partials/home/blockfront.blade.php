49049df9239a1f2a9551314c8d759c26f34231d6
Daniel Semblano <dsemblano@gmail.com>
2023-11-30 18:49:10 -0300

    border decreased bottom logos

--- site/web/app/themes/sfco179/resources/views/partials/home/blockfront.blade.php
+++ site/web/app/themes/sfco179/resources/views/partials/home/blockfront.blade.php
@@ -9,7 +9,7 @@
                     <img class="animate__animated animate__flipInX animate__delay-1s" width="160" height="128" src="@asset('images/sfco179_logo_branca.png')" alt="Logo SFCO 179">
                 </h1>
 
-                <div class="logos flex flex-wrap-reverse lg:flex-row-reverse justify-center items-center gap-8 border-white border-b-2 border-solid pb-6 lg:pb-10 lg:mt-24">
+                <div class="logos flex flex-wrap-reverse lg:flex-row-reverse justify-center items-center gap-8 border-white border-b border-solid pb-6 lg:pb-10 lg:mt-24">
                     <a class="w-1/4 lg:w-auto" href="/eventos"><img class="" width="80" height="79" src="@asset('images/logos/179_evnts.png')" alt="Logo SFCO 179" alt=""></a>
                     <a target="_blank" class="w-1/4 lg:w-auto" href="https://studiobrunosantos.com"><img class="" width="100" height="99" src="@asset('images/logos/bruno-santos-studio.png')" alt="Logo SFCO 179" alt=""></a>
                     <a target="_blank" class="w-1/4 lg:w-auto" href="https://utopiatropical.com.br"><img class="" width="100" height="99" src="@asset('images/logos/utopia-tropical-chocolates.png')" alt="Logo SFCO 179" alt=""></a>
