a424067ea15a5f23d55f30cee8f291e7fed31ba7
Daniel Semblano <dsemblano@gmail.com>
2024-06-19 21:17:27 -0300

    H1 fadedown animated

--- site/web/app/themes/sfco179/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sfco179/resources/views/partials/page-header.blade.php
@@ -1,3 +1,3 @@
 <div class="page-header not-prose mt-12 mb-4 md:mt-24 text-center text-6xl lg:text-8xl">
-  <h1>{!! $title !!}</h1>
+  <h1 class="animate__animated animate__fadeInDown animate__fast">{!! $title !!}</h1>
 </div>
