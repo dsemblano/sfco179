52dad08e407daf4dcc516fedbb38a73cd90e2f6b
Daniel Semblano <dsemblano@gmail.com>
2024-06-20 12:17:49 -0300

    404 page

--- site/web/app/themes/sfco179/app/View/Composers/Post.php
+++ site/web/app/themes/sfco179/app/View/Composers/Post.php
@@ -62,7 +62,7 @@ class Post extends Composer
         }
 
         if (is_404()) {
-            return __('Not Found', 'sage');
+            return __('Página não encontrada', 'sage');
         }
 
         return get_the_title();
