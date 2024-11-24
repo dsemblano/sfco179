a91252071eba4ecbfb680b0436885ffa6cb6d48d
Daniel Semblano <dsemblano@gmail.com>
2024-11-24 05:32:14 -0300

    Styling category page

--- site/web/app/themes/sfco179/resources/views/partials/content.blade.php
+++ site/web/app/themes/sfco179/resources/views/partials/content.blade.php
@@ -1,15 +1,20 @@
-<article @php(post_class())>
+<article @php(post_class('diaadia'))>
+  <a class="img-link lg:w-1/3" href="{{ get_permalink() }}">
+    <figure class="imgpost rounded">
+      {{ the_post_thumbnail('medium_large', array( 'class' => 'w-full rounded' ) ) }}
+    </figure>
+  </a>
   <header>
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        {!! $title !!}
-      </a>
-    </h2>
+      <h2 class="text-left mt-0">
+        <a class="postslinks" href="{{ get_permalink() }}">
+          {!! $title !!}
+        </a>
+      </h2>
 
-    @include('partials.entry-meta')
+      {{-- <p class="mb-3 excerpt">
+        <a class="postslinks" href="{{ get_permalink() }}">
+          {{ get_the_excerpt() }}
+        </a>
+      </p> --}}
   </header>
-
-  <div class="entry-summary">
-    @php(the_excerpt())
-  </div>
 </article>
