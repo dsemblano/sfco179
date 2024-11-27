660b6a3acaebe1b9fe79b6832b858fcf93d8fd9c
Daniel Semblano <dsemblano@gmail.com>
2024-11-27 11:02:18 -0300

    removed humans.txt plugin from composer.json, one humans.txt and link to public directory, all working well: humans.txt on public directory linked to root directory, header code fixed

--- site/web/app/themes/sfco179/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sfco179/resources/views/partials/page-header.blade.php
@@ -1,3 +1,11 @@
 <div class="page-header not-prose mt-12 mb-4 md:mt-24 text-center text-6xl lg:text-8xl">
-  <h1 class="animate__animated animate__fadeInDown animate__fast">{!! $title !!}</h1>
+  <h1 class="text-center mt-8 font-bold animate__animated animate__fadeInDown">{!! $title !!}</h1>
 </div>
+
+{{-- @php
+// Somente para a categoria Dia a Dia
+  $descricao_categoria = category_description();
+  if (!empty($descricao_categoria)) {
+      echo '<div id="categoria-descricao" class="text-center">' . $descricao_categoria . '</div>';
+  }
+@endphp  --}}
